c759eb5e9f4a5d9dd12aac3d1f30af30f7ea03d9
Dillon Nys <dnys1@asu.edu>
2020-07-08 13:29:54 -0700

    Add authentication

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// Github OAuth credentials
+const githubClientId = 'Iv1.0e8a9c6ed46a4df0';
+const githubClientSecret = '925d8197e70ed98bb6f4d488440d9d1fd83344ec';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
