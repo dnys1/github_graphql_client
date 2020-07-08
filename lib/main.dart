dd7aa244aa425c78230a231f4ba70bf901bdfe9b
Dillon Nys <dnys1@asu.edu>
2020-07-08 13:46:30 -0700

    Access Github's GraphQL API

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:gql_exec/gql_exec.dart';
+import 'package:gql_link/gql_link.dart';
+import 'package:gql_http_link/gql_http_link.dart';
 
 import 'github_oauth_credentials.dart';
+import 'src/github_gql/github_queries.data.gql.dart';
+import 'src/github_gql/github_queries.req.gql.dart';
 import 'src/github_login.dart';
 
 void main() {
@@ -31,18 +36,48 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return GithubLoginWidget(
       builder: (context, httpClient) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(title),
-          ),
-          body: Center(
-            child: Text('You are logged into Github!'),
-          ),
+        final link = HttpLink(
+          'https://api.github.com/graphql',
+          httpClient: httpClient,
+        );
+        return FutureBuilder<$ViewerDetail$viewer>(
+          future: viewerDetail(link),
+          builder: (context, snapshot) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text(title),
+              ),
+              body: Center(
+                child: Text(
+                  snapshot.hasData
+                      ? 'Hello ${snapshot.data.login}'
+                      : 'Retrieving viewer login details...',
+                ),
+              ),
+            );
+          },
         );
       },
       githubClientId: githubClientId,
       githubClientSecret: githubClientSecret,
       githubScopes: githubScopes,
     );
+  }
+}
+
+Future<$ViewerDetail$viewer> viewerDetail(Link link) async {
+  var result = await link.request(ViewerDetail((b) => b)).first;
+  if (result.errors != null && result.errors.isNotEmpty) {
+    throw QueryException(result.errors);
+  }
+  return $ViewerDetail(result.data).viewer;
+}
+
+class QueryException implements Exception {
+  QueryException(this.errors);
+  List<GraphQLError> errors;
+  @override
+  String toString() {
+    return 'QueryException: ${errors.map((err) => '$err').join(',')}';
   }
 }
